61fce1c7f9decd902664a6dce4805a8ae94327c0
rel1ght <relytmanion@gmail.com>
2023-02-22 14:58:22 -0600

    responsive

--- src/components/projects/workRow.js
+++ src/components/projects/workRow.js
@@ -10,6 +10,9 @@ import LeadsContent from "./content/leadsContent"
 import SakiContent from "./content/sakiContent"
 import PaveContent from "./content/paveContent"
 import WorkContext from "./context"
+import { SlArrowUp } from "react-icons/sl"
+import { Link as ScrollLink } from "react-scroll"
+
 function WorkRow() {
   const [currentContent, setCurrentContent] = useState("")
   const [activeThumbnail, setActiveThumbnail] = useState("")
@@ -47,14 +50,14 @@ function WorkRow() {
     <>
       <WorkContext.Provider value={{ contextValues }}>
         <div className="row justify-content-around">
-          <div className="col-3 col-xl-2">
+          <div className="col-4 col-xl-2">
             <WorkThumb
               src="./img/leadsthumbnail.jpg"
               title="LEADS"
               content={<LeadsContent />}
             />
           </div>
-          <div className="col-3 col-xl-2">
+          <div className="col-4 col-xl-2">
             <WorkThumb
               src="./img/sakithumbnail.jpg"
               title="SAKI Dashboard"
@@ -68,21 +71,21 @@ function WorkRow() {
               content={<PaveContent />}
             />
           </div> */}
-          <div className="col-3 col-xl-2">
+          <div className="col-4 col-xl-2">
             <WorkThumb
               src="./img/deepictthumbnail.png"
               title="Deepict"
               content={<DeepictContent />}
             />
           </div>
-          <div className="col-3 col-xl-2">
+          <div className="col-4 col-xl-2">
             <WorkThumb
               src="./img/mountainskygoldensthumbnail.png"
               title="Mountain Sky Goldens"
               content={<MsgContent />}
             />
           </div>
-          <div className="col-3 col-xl-2">
+          <div className="col-4 col-xl-2">
             <WorkThumb
               src="./img/fencethumbnail.png"
               title="Fence"
@@ -90,7 +93,7 @@ function WorkRow() {
             />
           </div>
 
-          <div className="col-3 col-xl-2">
+          <div className="col-4 col-xl-2">
             <WorkThumb
               src="./img/youromahathumbnail.jpg"
               title="Your Omaha"
@@ -103,6 +106,34 @@ function WorkRow() {
           className={`workContentRow row ${contentExpanded ? "" : "d-none"}`}
         >
           <WorkContent content={currentContent} />
+          <div className="w-100 d-flex d-lg-none">
+            <ScrollLink
+              isDynamic
+              currentClassName="is-current"
+              className="btn m-3 ml-auto blue-bg hoverShadow hoverLift"
+              to={"projects"}
+              spy={true}
+              smooth="easeInOutQuint"
+              offset={-100}
+              duration={1500}
+              // spyThrottle={500}
+              // onClick={handleClick}
+            >
+              Back to Projects{" "}
+              <div className="d-inline ml-1">
+                <SlArrowUp className="pb-1" />
+              </div>
+            </ScrollLink>
+          </div>
+          {/* <button
+            type="button"
+            className="btn m-3 ml-auto blue-bg hoverLift hoverShadow"
+          >
+            Back to Projects{" "}
+            <div className="d-inline ml-1">
+              <SlArrowUp className="pb-1" />
+            </div>
+          </button> */}
         </div>
       </WorkContext.Provider>
     </>
