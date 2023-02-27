5de819d1a1c348e5ccc4417654cf731d1564d04b
rel1ght <relytmanion@gmail.com>
2023-02-27 11:37:54 -0600

    added favicon png and previewcard metatags

--- src/components/head.js
+++ src/components/head.js
@@ -11,8 +11,34 @@ export default function Head(props) {
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
-      <meta name="description" content="" />
-      <meta name="author" content="" />
+      <meta name="description" content="It's like a resume, but nerdier!" />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://tylermanion.xyz/" />
+      <meta property="og:title" content="Tyler Manion's Portfolio Site" />
+      <meta
+        property="og:description"
+        content="It's like a resume, but nerdier!"
+      />
+      <meta
+        property="og:image"
+        content="https://tylermanion.xyz/img/faviconv1.png"
+      />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="tylermanion.xyz" />
+      <meta property="twitter:title" content="Tyler Manion's Portfolio Site" />
+      <meta
+        property="twitter:description"
+        content="It's like a resume, but nerdier!"
+      />
+      <meta
+        property="twitter:image"
+        content="https://tylermanion.xyz/img/faviconv1.png"
+      />
+      <meta name="author" content="Tyler Manion" />
       <link rel="icon" href={favicon} />
       <link
         href="https://fonts.googleapis.com/css?family=Rambla&display=swap"
